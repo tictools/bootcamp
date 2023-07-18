5503fa9d5ed2ed343223a4e6055ca977829526e0
tictools <jaumevillarreal@gmail.com>
2023-07-18 21:45:16 +0200

    feat: add map challenge and solutions

---
+++ array/api/01-map/challenge.js
@@ -0,0 +1,64 @@
+/**
+ * CONSTANTS
+ */
+const NUMBERS = [2, 5, 100];
+
+const NAMES = ["john", "JACOB", "jinGleHeimer", "schmidt"];
+
+const VIPS = [
+  {
+    name: "Foo",
+    age: 80,
+  },
+  {
+    name: "Bar",
+    age: 2,
+  },
+  {
+    name: "Fizz",
+    age: 5,
+  },
+  {
+    name: "Buzz",
+    age: 16,
+  },
+  {
+    name: "FizzBuzz",
+    age: 100,
+  },
+];
+
+/**
+ * Given NUMBERS array, define function 'transformNumbersToWords' to return an array of strings from numbers
+ *
+ * expected: ["2", "5", "100"]
+ */
+
+/**
+ * Given NAMES array, define function 'capitalizeNames' to return an array of strings with capitalized names
+ *
+ * expected: ["John", "Jacob", "Jingleheimer", "Schmidt"]
+ */
+
+/**
+ * Given VIPS array, define function 'namesOnly' to return an array of strings only with vips names
+ *
+ * expected: [ 'Foo', 'Bar', 'Fizz', 'Buzz', 'FizzBuzz' ]
+ */
+
+/**
+ * Given VIPS array, define function 'goToTheMatrix' to return an array of strings with a new message depending on the age
+ *
+ * if vips are over 18, they can go to the Matrix
+ * otherwise, they are under age
+ *
+ * expected: [ 'Foo can go to The Matrix', 'Bar is under age', 'Fizz is under age', 'Buzz is under age', 'FizzBuzz can go to The Matrix' ]
+ */
+
+/**
+ * Given VIPS array, define function 'appendVipsInDOM' to return an array of strings with vip name wrapped in tag <h1> and age wrapped in tag <h2>
+ * if vips is over 18, they can go to the Matrix
+ * otherwise, they are under age
+ *
+ * expected: [ '<h1>Foo</h1><h2>80</h2>', '<h1>Bar</h1><h2>2</h2>', '<h1>Fizz</h1><h2>5</h2>', '<h1>Buzz</h1><h2>16</h2>', '<h1>FizzBuzz</h1><h2>100</h2>' ]
+ */
