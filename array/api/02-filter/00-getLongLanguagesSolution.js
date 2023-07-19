a3c0313675c611587bd57c5accf10b84c821d2c5
tictools <jaumevillarreal@gmail.com>
2023-07-19 18:51:47 +0200

    feat: refine exercise descriptions

--- array/api/02-filter/00-getLongLanguagesSolution.js
+++ array/api/02-filter/00-getLongLanguagesSolution.js
@@ -5,7 +5,6 @@ const languagesList = ["Java", "C++", "JavaScript", "C#", "TypeScript"];
 
 /**
  * Given 'wordsList' array, define function 'extractLongWords' to return an array only with strings with length equal or greater then 5
- * use for() instruction
  *
  * expected: ['JavaScript', 'TypeScript']
  */
